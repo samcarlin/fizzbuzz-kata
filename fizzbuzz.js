bfeaa7b06c3f01142e591f679b0f73ff3d12f7e0
Samantha Carlin <samcarlin@samanthas-mbp.home>
2019-04-06 22:23:29 -0400

    fizz buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,24 @@
-function fizzbuzz() {
-    return 1
+const FIZZ = "fizz"
+const BUZZ = "buzz"
+
+function fizzbuzz(input) {
+    if (isMultipleOf(input, 3) && (isMultipleOf(input, 5))) {
+        return FIZZ + BUZZ
+    }
+
+    if (isMultipleOf(input, 3)) {
+        return FIZZ
+    }
+
+    if (isMultipleOf(input, 5)) {
+        return BUZZ
+    }
+
+    return input.toString()
+}
+
+function isMultipleOf(dividend, divisor) {
+    return dividend % divisor === 0
 }
 
 module.exports = fizzbuzz
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,6 +3,56 @@ const fizzbuzz = require('./fizzbuzz')
 describe('fizzbuzz', () => {
     test('returns 1 when fizzbuzz of 1', () => {
         let result = fizzbuzz(1)
-        expect(result).toBe(1)
+        expect(result).toBe("1")
+    })
+
+    test('returns 2 when fizzbuzz of 2', () => {
+        let result = fizzbuzz(2)
+        expect(result).toBe("2")
+    })
+
+    test('returns fizz when fizzbuzz of 3', () => {
+        let result = fizzbuzz(3)
+        expect(result).toBe("fizz")
+    })
+
+    test('returns 4 when fizzbuzz of 4', () => {
+        let result = fizzbuzz(4)
+        expect(result).toBe("4")
+    })
+
+    test('returns buzz when fizzbuzz of 5', () => {
+        let result = fizzbuzz(5)
+        expect(result).toBe("buzz")
+    })
+
+    test('returns fizz when fizzbuzz of 6', () => {
+        let result = fizzbuzz(6)
+        expect(result).toBe("fizz")
+    })
+
+    test('returns fizz when fizzbuzz of 9', () => {
+        let result = fizzbuzz(9)
+        expect(result).toBe("fizz")
+    })
+
+    test('returns buzz when fizzbuzz of 10', () => {
+        let result = fizzbuzz(10)
+        expect(result).toBe("buzz")
+    })
+
+    test('returns fizzbuzz when fizzbuzz of 15', () => {
+        let result = fizzbuzz(15)
+        expect(result).toBe("fizzbuzz")
+    })
+
+    test('returns fizzbuzz when fizzbuzz of 30', () => {
+        let result = fizzbuzz(30)
+        expect(result).toBe("fizzbuzz")
+    })
+
+    test('returns buzz when fizzbuzz of 100', () => {
+        let result = fizzbuzz(100)
+        expect(result).toBe("buzz")
     })
 })
